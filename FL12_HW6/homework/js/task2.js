bd5c86cdc8bcc9626ca7e06e94161d470ac118af
dvvukr-project <dvvukr@gmail.com>
2019-12-20 12:12:17 +0200

    js task

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,9 @@
-// Your code goes here
+var a = prompt("Enter the side of triangle A:", '');
+var b = prompt("Enter the side of triangle B:", '');
+var c = prompt("Enter the side of triangle C:", '');
+
+
+console.log("Triangle doesn’t exist:", a);
+console.log("Equilateral triangle:", b);
+console.log("Isosceles triangle:", c);
+console.log("Scalene triangle:", a+b+c);
