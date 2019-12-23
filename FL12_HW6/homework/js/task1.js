3d3ae0b383fbc4fa363e6328a8900d21110284cf
dvvukr-project <dvvukr@gmail.com>
2019-12-23 19:45:09 +0200

    js hw fix

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,26 @@
+let a = parseInt(prompt('Enter value A:', 1));
+console.log('Value A:' + a);
+let b = parseInt(prompt('Enter value B:', 1));
+console.log('Value B:' + b);
+let c = parseInt(prompt('Enter value C:', 1));
+console.log('Value C:' + c);
 
+if (!a || a === 0 || isNaN(a)) {
+	alert('Invalid input data (a)');
+} else if (!b || b === 0 || isNaN(b)){
+	alert('Invalid input data (b)');
+} else if (!c || c === 0 || isNaN(c)){
+	alert('Invalid input data (c)');
+} else {
+	let Descriminant = b*b - 4*a *c;
+	if (Descriminant < 0) {
+		console.log('no solution');
+	} else if (Descriminant === 0) {
+		console.log('x = 0');
+	} else {
+		let x1 =-b - Math.sqrt(Descriminant)/2/a;
+		let x2 =-b + Math.sqrt(Descriminant)/2/a;
+		console.log('x1 = ' + x1);
+		console.log('x2 = ' + x2);
+	}
+}
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,9 +1,33 @@
-var a = prompt("Enter the side of triangle A:", '');
-var b = prompt("Enter the side of triangle B:", '');
-var c = prompt("Enter the side of triangle C:", '');
+let a = prompt('Enter the side of triangle A:');
+console.log('Side A:', a);
+let b = prompt('Enter the side of triangle B:');
+console.log('Side B:', b);
+let c = prompt('Enter the side of triangle C:');
+console.log('Side C:', c);
 
-
-console.log("Triangle doesn’t exist:", a);
-console.log("Equilateral triangle:", b);
-console.log("Isosceles triangle:", c);
-console.log("Scalene triangle:", a+b+c);
+if (a === '' || a === undefined || (b === '' || b === undefined) || (c === '' || c === undefined)) {
+	alert('Input values should be ONLY numbers');
+	console.log('Input values should be ONLY numbers');
+} else if (a === 0 || b === 0 || c === 0) {
+	alert('Triangle must have 3 sides with a positive definite length');
+	console.log('Triangle must have 3 sides with a positive definite length');
+} else {
+	a = parseInt(a, 10);
+	b = parseInt(b, 10);
+	c = parseInt(c, 10);
+	if (isNaN(a) && !(a + b > c) || isNaN(b) && !(a + c > b) || isNaN(c) && !(b + c > a)) {
+		alert('Triangle doesn’t exist');
+		console.log('Triangle doesn’t exist');
+	} else {
+		if (a === b && a === c) {
+			alert('Equilateral triangle');
+			console.log('Equilateral triangle');
+		} else if (a === b && a !==c || a === c && a !==b || b === c && b !==a) {
+			alert('Isosceles triangle');
+			console.log('Isosceles triangle');
+		} else {
+			alert('Scalene triangle');
+			console.log('Scalene triangle');
+		}
+	}
+}
