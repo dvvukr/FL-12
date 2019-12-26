65238f8c0dfdcc71566ec094b10da3210d60fd77
dvvukr-project <dvvukr@gmail.com>
2019-12-26 14:08:40 +0200

    valid fix

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,26 +1,26 @@
-var userEmail = prompt('Enter your email:', '');
+let userEmail = prompt('Enter your email:', '');
 
-if ( userEmail == '' || userEmail == null ) {
+if ( userEmail === '' || userEmail === null ) {
 	alert('Canceled.');	
 } else if ( userEmail.length < 5 ) {
 	alert('I don\'t know any emails having name length less than 5 symbols');
-} else if (  userEmail === 'user@gmail.com' ) {
-	var password = 'UserPass';
-	var userPassword = prompt('Enter your password:', '');
+} else if ( userEmail === 'user@gmail.com' ) {
+	let password = 'UserPass';
+	let userPassword = prompt('Enter your password:', '');
 	if ( userPassword === '' || userPassword === null ) {
 		alert('Canceled.');	
 	} else if (userPassword === password) {
-		var needToChange = confirm('Do you want to change your password?');
+		let needToChange = confirm('Do you want to change your password?');
 		if (needToChange) {
-			var oldPassword = prompt('Enter your old password:', '');
+			let oldPassword = prompt('Enter your old password:', '');
 			if (oldPassword === password) {
-				var newPassword = prompt('Enter new password.', '');
+				let newPassword = prompt('Enter new password.', '');
 				if ( newPassword === '' || newPassword === null ) {
 					alert('Canceled.');
 				} else if ( newPassword.length < 6 ) {
 					alert('It’s too short password. Sorry.');
 				} else {
-					var newPassword2 = prompt('Repeat your new password.', '');
+					let newPassword2 = prompt('Repeat your new password.', '');
 					if (newPassword2 === newPassword) {
 						alert('You have successfully changed your password.');
 					} else {
@@ -37,22 +37,22 @@ if ( userEmail == '' || userEmail == null ) {
 		alert('Wrong password.');	
 	}
 } else if ( userEmail === 'admin@gmail.com' ) {
-	var password = 'AdminPass';
-	var adminPassword = prompt('Enter your password:', '');
+	let password = 'AdminPass';
+	let adminPassword = prompt('Enter your password:', '');
 	if ( adminPassword === '' || adminPassword === null ) {
 		alert('Canceled.');	
 	} else if (adminPassword === password) {
-		var needToChange = confirm('Do you want to change your password?');
+		let needToChange = confirm('Do you want to change your password?');
 		if (needToChange) {
-			var oldPassword = prompt('Enter your old password:', '');
+			let oldPassword = prompt('Enter your old password:', '');
 			if (oldPassword === password) {
-				var newPassword = prompt('Enter new password.', '');
+				let newPassword = prompt('Enter new password.', '');
 				if ( newPassword === '' || newPassword === null ) {
 					alert('Canceled.');
 				} else if ( newPassword.length < 6 ) {
-					alert('It\’s too short password. Sorry.');
+					alert('It’s too short password. Sorry.');
 				} else {
-					var newPassword2 = prompt('Repeat your new password.', '');
+					let newPassword2 = prompt('Repeat your new password.', '');
 					if (newPassword2 === newPassword) {
 						alert('You have successfully changed your password.');
 					} else {
@@ -69,5 +69,5 @@ if ( userEmail == '' || userEmail == null ) {
 		alert('Wrong password.');	
 	}
 } else {
-	alert('I don\’t know you');
+	alert('I don’t know you');
 }
