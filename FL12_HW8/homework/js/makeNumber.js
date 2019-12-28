25f99a4afbc3801fa2d2680ec8380da572d3a714
dvvukr-project <dvvukr@gmail.com>
2019-12-28 17:37:07 +0200

    makeNmb

--- FL12_HW8/homework/js/makeNumber.js
+++ FL12_HW8/homework/js/makeNumber.js
@@ -1,6 +1,11 @@
-function makeNumber(){
-	
-	return;
+function makeNumber(string){
+	var number = '';
+	for (var i = 0; i <= string.length; i++) {
+        if (Number.isInteger(parseInt(string[i]))) {
+            number = number + string[i];
+        }
+    }
+	return number;
 }
 makeNumber('erer384jjjfd123'); //=>384123
 makeNumber('123098h76gfdd'); //=>12309876
